6b547f48f714360695e6d37d95548a8166638dbf
L.Robyns <lucasrobyns@hotmail.com>
2020-08-08 18:00:27 +0200

    Service class modification

---
+++ src/com/projet/services/Service.java
@@ -0,0 +1,53 @@
+package com.projet.services;
+
+import com.projet.dao.EntityFinder;
+import com.projet.dao.EntityFinderImpl;
+import com.projet.entities.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceContext;
+
+/**
+ * =================================================================
+ * Created by Intellij IDEA.
+ *
+ * @author lucas
+ * @project Projet TFE
+ * Date: 07/08/2020
+ * Time: 20:14
+ * =================================================================
+ */
+public abstract class Service<E> implements IService<E> {
+
+    @PersistenceContext(unitName = "jsf_tfe")
+    private EntityManager em;
+
+    EntityFinder<E> finder;
+
+    Service(Class<?> ec) {
+        finder = new EntityFinderImpl<>(ec, this.em);
+    }
+
+    public E getById (int id) {
+        return finder.findOne(id);
+    }
+
+    public abstract E save(E e);
+
+    public void delete(E e) {
+        if (em.contains(e)) {
+            em.merge(e);
+        }
+
+        em.remove(e);
+    }
+
+    public EntityTransaction getTransaction() {
+        return em.getTransaction();
+    }
+
+    public void close() {
+        em.close();
+    }
+}
--- src/com/projet/services/UserService.java
+++ src/com/projet/services/UserService.java
@@ -9,6 +9,7 @@ import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.credential.PasswordMatcher;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.List;
@@ -24,113 +25,26 @@ import java.util.Map;
  * Time: 18:00
  * =================================================================
  */
-public class UserService implements  IService<User>, Serializable {
+public class UserService extends Service<User> implements Serializable {
     private static final Logger log = Logger.getLogger(UserService.class);
     private static final long serialVersionUID = 1L;
 
-    private User userDB;
+    @PersistenceContext(unitName = "jsf_tfe")
     private EntityManager em;
 
-    public UserService(EntityManager em) {
-        this.em = em;
-    }
-
-    public UserService() {
-
+    public UserService(Class<?> ec) {
+        super(ec);
     }
 
     public List<User> getAll() {
-        EntityFinder<User> finder = new EntityFinderImpl<>(User.class);
         return finder.findByNamedQuery("User.findAll", null);
     }
 
     public User getByUsername(String username) {
         Map<String, String> param = new HashMap<>();
         param.put("username", username);
-        EntityFinder<User> finder = new EntityFinderImpl<>(User.class);
 
         return finder.findOneByNamedQuery("User.findUserByUsername", param);
-    }
-
-    public User getById(Integer id) {
-        return null;
-    }
-
-    public boolean create(User user) {
-        em = EMF.getEM();
-        em.getTransaction().begin();
-        boolean status = false;
-        try {
-            PasswordMatcher auth = new PasswordMatcher();
-            this.userDB.setPassword(auth.getPasswordService().encryptPassword(user.getPassword()));
-            em.persist(user);
-            em.getTransaction().commit();
-            status = true;
-        } finally {
-            if (em.getTransaction().isActive()) {
-                em.getTransaction().rollback();
-                log.error("Rollback");
-                status = false;
-            }
-            em.close();
-            log.debug("Ok");
-        }
-        return status;
-    }
-
-    public boolean update(User user) throws Exception {
-        em = EMF.getEM();
-        em.getTransaction().begin();
-        boolean status = false;
-        try {
-            this.userDB = em.find(User.class, user.getId());
-            if (this.userDB != null) {
-                PasswordMatcher auth = new PasswordMatcher();
-                this.userDB.setPassword(auth.getPasswordService().encryptPassword(user.getPassword()));
-                this.userDB.setEmail(user.getEmail());
-                this.userDB.setFirstName(user.getFirstName());
-                this.userDB.setLastName(user.getLastName());
-                this.userDB.setUsername(user.getUsername());
-                em.merge(userDB);
-                em.getTransaction().commit();
-                status = true;
-            }
-        }finally {
-            if (em.getTransaction().isActive()) {
-                em.getTransaction().rollback();
-                log.error("Rollback");
-                status = false;
-            }
-            em.close();
-            log.debug("Ok");
-        }
-        return status;
-    }
-
-    public boolean deleteByID(User user) {
-        em = EMF.getEM();
-        em.getTransaction().begin();
-        boolean status = false;
-        try{
-            User userDB = em.find(User.class, user.getId());
-            if (userDB != null) {
-                em.remove(userDB);
-                status = true;
-            } else {
-                status = false;
-            }
-            em.getTransaction().commit();
-
-        } finally {
-            if (em.getTransaction().isActive()) {
-                em.getTransaction().rollback();
-                log.error("Rollback");
-                status = false;
-            }
-            em.close();
-            log.debug("Ok");
-        }
-        return status;
     }
 
     @Override
@@ -141,14 +55,5 @@ public class UserService implements  IService<User>, Serializable {
             user = em.merge(user);
         }
         return user;
-    }
-
-    @Override
-    public void delete(User user) {
-        if (!em.contains(user)) {
-            em.merge(user);
-        }
-
-        em.remove(user);
     }
 }
