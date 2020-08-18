13b50714b6e90392ebe86cfa67f108b4b4c59042
Amaury <amaury.lapaque@gmail.com>
2020-08-18 19:54:36 +0200

    dashboardworks

--- src/com/projet/controllers/DashboardBean.java
+++ src/com/projet/controllers/DashboardBean.java
@@ -11,7 +11,6 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import java.io.Serializable;
-import java.lang.reflect.Array;
 import java.util.List;
 
 @Named("dashboard")
