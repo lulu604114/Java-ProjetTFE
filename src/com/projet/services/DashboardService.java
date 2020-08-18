385f8f716bf652af1bc21a4fa52d39beef22f78c
Amaury <amaury.lapaque@gmail.com>
2020-08-18 19:22:48 +0200

    dashboardworks

--- src/com/projet/services/DashboardService.java
+++ src/com/projet/services/DashboardService.java
@@ -5,9 +5,6 @@ import com.projet.entities.User;
 import org.apache.log4j.Logger;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class DashboardService extends Service<Dashboard> implements Serializable {
     private static final Logger log = Logger.getLogger(DashboardService.class);
